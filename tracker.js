958e5535e87cea78d3dd19dfe513fbce2eb36c34
douglch <douglchwm@gmail.com>
2022-12-18 20:55:27 +0800

    Announce req and resp functions

--- tracker.js
+++ tracker.js
@@ -4,6 +4,7 @@ const dgram = require('dgram');
 const Buffer = require('buffer').Buffer;
 const urlParse = require('url').parse;
 const crypto = require('crypto'); // For creating a random number for the buffer
+const torrentParser = require('./torrent-parser');
 
 module.exports.getPeers = (torrent, callback) => {
     /* dgram.createSocket
@@ -28,7 +29,7 @@ module.exports.getPeers = (torrent, callback) => {
           // 2. receive and parse connect response 
           const connResp = parseConnResp(response);
           // 3. send announce request
-          const announceReq = buildAnnounceReq(connResp.connectionId);
+          const announceReq = buildAnnounceReq(connResp.connectionId, torrent);
           udpSend(socket, announceReq, url);
         } else if (respType(response) === 'announce') {
           // 4. parse announce response
@@ -78,10 +79,61 @@ function respType(resp) {
     }
   }
   
-  function buildAnnounceReq(connId) {
-    // ...
+
+  function buildAnnounceReq(connId, torrent, port=6881) {
+    // Official spec says that the ports for bittorrent should be between 6881 and 6889.
+    const buf = Buffer.allocUnsafe(98);   
+    
+    // Just follow the announce request format and copy into the buffer :)
+    // connection id
+    connId.copy(buf, 0);
+    // action
+    buf.writeUInt32BE(1, 8);
+    // transaction id
+    crypto.randomBytes(4).copy(buf, 12);
+    // info hash
+    torrentParser.infoHash(torrent).copy(buf, 16);
+    // peerId
+    util.genId().copy(buf, 36);
+    // downloaded (copying buffer into buffer)
+    Buffer.alloc(8).copy(buf, 56);
+    // left
+    torrentParser.size(torrent).copy(buf, 64);
+    // uploaded 
+    Buffer.alloc(8).copy(buf, 72);
+    // event
+    buf.writeUInt32BE(0, 80);
+    // ip address
+    buf.writeUInt32BE(0, 80);
+    // key
+    crypto.randomBytes(4).copy(buf, 88);
+    // num want (signed)
+    buf.writeInt32BE(-1, 92);
+    // port
+    buf.writeUInt16BE(port, 96);
+
+    return buf;
   }
   
   function parseAnnounceResp(resp) {
-    // ...
+    // for grouping n ip and port objects
+    function group(iterable, groupSize) {
+        let groups = [];
+        for (let i = 0; i < iterable.length; i += groupSize) {
+          groups.push(iterable.slice(i, i + groupSize));
+        }
+        return groups;
+    }
+    return {
+        action: resp.readUInt32BE(0),
+        transactionId: resp.readUInt32BE(4),
+        leechers: resp.readUInt32BE(8),
+        seeders: resp.readUInt32BE(12),
+        peers: group(resp.slice(20), 6).map(address => {
+          return {
+            ip: address.slice(0, 4).join('.'),
+            port: address.readUInt16BE(4)
+          }
+        })
+      }
   }
